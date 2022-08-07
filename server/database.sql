1dfcb4a806ff4b73763f703b6282f6adbb0ee39e
alina <mlkysheikh@gmail.com>
2022-08-07 18:15:07 -0400

    Squashed commit of the following:
    commit 98de5a4c58819076c4b70779487110cb6e84a3d6
    Author: alina <mlkysheikh@gmail.com>
    Date:   Sun Aug 7 18:11:17 2022 -0400
    
        Update index.js
    
    commit 6f6cd1ab2d9c85d143f741eb6ab22c8769f4d667
    Author: alina <mlkysheikh@gmail.com>
    Date:   Sun Aug 7 18:05:28 2022 -0400
    
        Create new project and set up routes.

---
+++ server/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE notesdb;
+
+CREATE TABLE notes(
+    note_id SERIAL PRIMARY KEY,
+    title VARCHAR (255) NOT NULL,
+    description TEXT,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
